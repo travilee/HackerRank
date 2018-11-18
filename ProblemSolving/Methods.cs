534c73e45a64401c522cac581049981ae79f9fbd
DESKTOP-J0H0BVA\Student <travisleebarnhart@gmail.com>
2018-11-18 16:20:49 -0600

    Added SumTwoIntegers

---
+++ ProblemSolving/Methods.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving
+{
+    public class Methods
+    {
+        public static void SumTwoIntegers()
+        {
+            //Complete the function solveMeFirst to compute the sum of two integers.
+
+            //Function prototype:
+            //int solveMeFirst(int a, int b);
+            //where,
+            //a is the first integer input.
+            //b is the second integer input
+            //Return values sum of the above two integers
+
+            Console.Write("Enter first integer: ");
+            int numb1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter second integer: ");
+            int numb2 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"The sum of {numb1:n0} and {numb2:n0} is {(numb1 + numb2):n0}.\n");
+        }
+
+    }
+}
--- ProblemSolving/Program.cs
+++ ProblemSolving/Program.cs
@@ -10,6 +10,32 @@ namespace ProblemSolving
     {
         static void Main(string[] args)
         {
+            bool exit = false;
+            do
+            {
+                Console.WriteLine("Choose an option from below:\n" +
+                                "1) Add two integers\n" +
+                                "X) Exit");
+                string choice = Console.ReadLine();
+                Console.Clear();
+
+                switch (choice)
+                {
+                    case "1":
+                        Console.WriteLine("Add two integers.\n");
+                        Methods.SumTwoIntegers();
+                        break;
+                    case "x":
+                    case "X":
+                        Console.WriteLine("Thanks for visiting!!\n");
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option. Please try again.\n");
+                        break;
+                }
+            } while (!exit);
+
         }
     }
 }
